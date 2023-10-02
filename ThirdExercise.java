2976ce2859e0d15cb940728f083c6ca5373627fb
Angelo Moreno <emilioangelo665@gmail.com>
2023-10-02 15:50:39 +0200

    Metodo pari o dispari fatto

--- ThirdExercise.java
+++ ThirdExercise.java
@@ -12,10 +12,23 @@ public class ThirdExercise {
         double result1 = rectanglePerimeter(width, height);
         System.out.println("Result: " + result1);
 
+        System.out.println("--------------- Exercise 2 ---------------");
+        System.out.println("--> Program return 0 (EVEN) else 1 (ODD)");
+        System.out.println("· Write your number");
+        int num1 = Integer.parseInt(input.nextLine());
+        int result2 = evenOrOdd(num1);
+        System.out.println("Result: " + result2 + (result2 == 0 ? " (EVEN)" : " (ODD)"));
         input.close();
 
     }
     public static double rectanglePerimeter(double width, double height){
         return width * height;
+    }
+    public static int evenOrOdd(int num){
+        if(num % 2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 }
