e49663c9536406bf20724eb1c9b0344b4a098dda
Angelo Moreno <emilioangelo665@gmail.com>
2023-10-02 14:46:02 +0200

    Corretto scorso met. e fatto metodo concatena

--- FirstExercise.java
+++ FirstExercise.java
@@ -2,21 +2,31 @@ import java.util.Scanner;
 
 public class FirstExercise {
     public static void main(String[] args) {
-        System.out.println("--------------- Exercise 1 ---------------");
-        multiplication();
-        System.out.println("-------------------------------------------");
-
-
-    }
-
-    public static void multiplication() {
         Scanner input = new Scanner(System.in);
+        System.out.println("--------------- Exercise 1 ---------------");
         System.out.println("--> Program to multiply two numbers");
         System.out.println("· Write your first number");
-        int num1 = input.nextInt();
+        int num1 = Integer.parseInt(input.nextLine());
         System.out.println("· Write your second number");
-        int num2 = input.nextInt();
-        input.close();
-        System.out.println("Result: " + (num1 * num2));
+        int num2 = Integer.parseInt(input.nextLine());
+        int result1 = multiplication(num1, num2);
+        System.out.println("Result: " + result1);
+        System.out.println("--------------- Exercise 2 ---------------");
+        System.out.println("--> Program to concat a string with a number");
+        System.out.println("· Write your string");
+        String str1 = input.nextLine();
+        System.out.println("· Write your number");
+        int num3 = Integer.parseInt(input.nextLine());
+        String result2 = concatena(str1, num3);
+        System.out.println("Result: " + result2);
+        System.out.println("-------------------------------------------");
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return num1 *  num2;
+    }
+
+    public static String concatena(String str1, int num1) {
+        return str1  + num1;
     }
 }
